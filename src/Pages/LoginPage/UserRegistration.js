8fa286aac2291b9a4d4129c592c59986596fb660
Nehara831 <112460919+Nehara831@users.noreply.github.com>
2023-09-30 00:12:16 +0530

    Registration page added

--- src/Pages/LoginPage/UserRegistration.js
+++ src/Pages/LoginPage/UserRegistration.js
@@ -1,81 +1,151 @@
-import React, {
-    Component
-} from 'react';
-import { Link} from 'react-router-dom'; 
-import { Form,Button, Grid, Segment, Divider} from 'semantic-ui-react';
-import axios from 'axios';
+// import React, {
+//     Component
+// } from 'react';
+// import { Link} from 'react-router-dom'; 
+// import { Form,Button, Grid, Segment, Divider} from 'semantic-ui-react';
+// import axios from 'axios';
 
-/* ................................ 
-.........Main App class ...........
-..................................*/
+// /* ................................ 
+// .........Main App class ...........
+// ..................................*/
                
-class UserRegister extends Component {
+// class UserRegister extends Component {
     
-    constructor(props){
-        super(props);
-        this.state={
-            username:'',
-            password:'',
-            email:'',
-            contact:'',
-            description:'',
-            first_name:'',
-            last_name:'',
-            middle_name:'',
-            location:'',
-            dob:''
-        };
-        this.register=this.register.bind(this);
-    }
+//     constructor(props){
+//         super(props);
+//         this.state={
+//             username:'',
+//             password:'',
+//             email:'',
+//             contact:'',
+//             description:'',
+//             first_name:'',
+//             last_name:'',
+//             middle_name:'',
+//             location:'',
+//             dob:''
+//         };
+//         this.register=this.register.bind(this);
+//     }
     
-    handleChange = (e, { name, value }) => this.setState({ [name]: value })
+//     handleChange = (e, { name, value }) => this.setState({ [name]: value })
   
-    render() {
-        const { username, password,email,contact,description, first_name,last_name,middle_name,location,dob } = this.state;
-        return ( 
-        <Grid  centered >
-        <Grid.Column width={12} >
-        <Segment inverted >
-        <Form inverted >
-            <Form.Group widths="equal">
-            <Form.Input label='Username' placeholder='Username' name="username" value={username} onChange={this.handleChange}/>
-            <Form.Input label='Password' placeholder='Password' type='password' name="password" value={password} onChange={this.handleChange}/>
-            </Form.Group>
-            <Form.Input label='Email' placeholder='Email' name="email" value={email} onChange={this.handleChange}/>
-            <Form.Input label='Contact number' placeholder='Contact' name="contact" value={contact} onChange={this.handleChange}/>
-            <Form.Input label='Description' placeholder='description' name="description" value={description} onChange={this.handleChange}/>
-            <Form.Group widths="equal">
-            <Form.Input label='First Name' placeholder='First Name' name="first_name" value={first_name} onChange={this.handleChange}/>
-            <Form.Input label='Middle Name' placeholder='Middle Name' name="middle_name" value={middle_name} onChange={this.handleChange}/>
-            <Form.Input label='Last Name' placeholder='Last Name' name="last_name" value={last_name} onChange={this.handleChange}/>
-            </Form.Group>
-            <Form.Input label='Location' placeholder='Location' name="location" value={location} onChange={this.handleChange}/>
-            <Form.Input label='Date of Birth (DD/MM/YYYY)' placeholder='DD/MM/YYYY' name="dob" value={dob} onChange={this.handleChange}/>
+//     render() {
+//         const { username, password,email,contact,description, first_name,last_name,middle_name,location,dob } = this.state;
+//         return ( 
+//         <Grid  centered >
+//         <Grid.Column width={12} >
+//         <Segment inverted >
+//         <Form inverted >
+//             <Form.Group widths="equal">
+//             <Form.Input label='Username' placeholder='Username' name="username" value={username} onChange={this.handleChange}/>
+//             <Form.Input label='Password' placeholder='Password' type='password' name="password" value={password} onChange={this.handleChange}/>
+//             </Form.Group>
+//             <Form.Input label='Email' placeholder='Email' name="email" value={email} onChange={this.handleChange}/>
+//             <Form.Input label='Contact number' placeholder='Contact' name="contact" value={contact} onChange={this.handleChange}/>
+//             <Form.Input label='Description' placeholder='description' name="description" value={description} onChange={this.handleChange}/>
+//             <Form.Group widths="equal">
+//             <Form.Input label='First Name' placeholder='First Name' name="first_name" value={first_name} onChange={this.handleChange}/>
+//             <Form.Input label='Middle Name' placeholder='Middle Name' name="middle_name" value={middle_name} onChange={this.handleChange}/>
+//             <Form.Input label='Last Name' placeholder='Last Name' name="last_name" value={last_name} onChange={this.handleChange}/>
+//             </Form.Group>
+//             <Form.Input label='Location' placeholder='Location' name="location" value={location} onChange={this.handleChange}/>
+//             <Form.Input label='Date of Birth (DD/MM/YYYY)' placeholder='DD/MM/YYYY' name="dob" value={dob} onChange={this.handleChange}/>
             
             
-            <Button type='submit' onClick={this.register}>Register</Button>
+//             <Button type='submit' onClick={this.register}>Register</Button>
             
-          </Form>
-          </Segment>
-          </Grid.Column>
-          </Grid>  
-        );
-    }
+//           </Form>
+//           </Segment>
+//           </Grid.Column>
+//           </Grid>  
+//         );
+//     }
 
-    register(){
+//     register(){
         
-        console.log(JSON.stringify(this.state));
-        axios.post(config.get('base_url')+this.props.match.url,this.state)
-          .then(function (response) {
-            console.log(response);
+//         console.log(JSON.stringify(this.state));
+//         axios.post(config.get('base_url')+this.props.match.url,this.state)
+//           .then(function (response) {
+//             console.log(response);
             
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-    }
+//           })
+//           .catch(function (error) {
+//             console.log(error);
+//           });
+//     }
         
-}
+// }
 
 
-export default UserRegister;
+// export default UserRegister;
+
+import React, { useState } from 'react';
+
+import { PlusOutlined } from '@ant-design/icons';
+import './UserRegistration.css'
+
+import {
+  Button,
+  Cascader,
+  Checkbox,
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+  Radio,
+  Select,
+  Slider,
+  Switch,
+  TreeSelect,
+  Upload,
+} from 'antd';
+
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
+
+const normFile = (e) => {
+  if (Array.isArray(e)) {
+    return e;
+  }
+  return e?.fileList;
+};
+
+const FormDisabledDemo = () => {
+  const [componentDisabled, setComponentDisabled] = useState(true);
+
+  return (
+    <>
+    <div className='Form'>
+      
+      <Form
+        labelCol={{ span: 14 }}
+        wrapperCol={{ span: 25 }}
+        layout="vertical"
+        disabled={false}
+        style={{ maxWidth: 600 }}
+      >
+       
+        <Form.Item label="First Name">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Last Name">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Email Address">
+          <Input />
+        </Form.Item>
+        <Form.Item label="PassWord">
+          <Input />
+        </Form.Item>
+        <Form.Item >
+          <Button>Create an Account</Button>
+        </Form.Item>
+      </Form>
+      </div>
+    </>
+    
+  );
+};
+
+export default () => <FormDisabledDemo />;
